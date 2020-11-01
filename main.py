08588f21f094fbf0be32945bf4c2b36a4234d699
Haknazar117 <67150813+Haknazar117@users.noreply.github.com>
2020-11-01 18:45:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,51 @@
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
 
-from plyer import stt
+import speech_recognition as sr
 
-Builder.load_string('''
-#:import stt plyer.stt
-<SpeechInterface>:
-    orientation: 'vertical'
-    Label:
-        size_hint_y: None
-        height: sp(40)
-        text: 'Is supported: %s' % stt.exist()
-    Label:
-        size_hint_y: None
-        height: sp(40)
-        text: 'Possible Matches'
-    TextInput:
-        id: results
-        hint_text: 'results (auto stop)'
-    TextInput:
-        id: partial
-        hint_text: 'partial results (manual stop)'
-    TextInput:
-        id: errors
-        hint_text: 'errors'
-    Button:
-        id: start_button
-        text: 'Start Listening'
-        on_release: root.start_listening()
-''')
+r = sr.Recognizer()
+m = sr.Microphone()
 
 
-class SpeechInterface(BoxLayout):
-    '''Root Widget.'''
+# Root Widget
+class Root(BoxLayout):
+    pass
 
-    def start_listening(self):
-        if stt.listening:
-            self.stop_listening()
-            return
 
-        start_button = self.ids.start_button
-        start_button.text = 'Stop'
-
-        self.ids.results.text = ''
-        self.ids.partial.text = ''
-
-        stt.start()
-
-        Clock.schedule_interval(self.check_state, 1 / 5)
-
-    def stop_listening(self):
-        start_button = self.ids.start_button
-        start_button.text = 'Start Listening'
-
-        stt.stop()
-        self.update()
-
-        Clock.unschedule(self.check_state)
-
-    def check_state(self, dt):
-        # if the recognizer service stops, change UI
-        if not stt.listening:
-            self.stop_listening()
-
-    def update(self):
-        self.ids.partial.text = '\n'.join(stt.partial_results)
-        self.ids.results.text = '\n'.join(stt.results)
+class RecordButton(Button):
+    # String Property to Hold output for publishing by Textinput
+    output = StringProperty('')
+    
+    def record(self):
+        # GUI Blocking Audio Capture
+        with m as source:
+            audio = r.listen(source)
+        
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
+            self.output = "You said \"{}\"".format(value)
+        
+        except sr.UnknownValueError:
+            self.output = ("Oops! Didn't catch that")
+        
+        except sr.RequestError as e:
+            self.output = ("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 
 
 class SpeechApp(App):
-
     def build(self):
-        return SpeechInterface()
+        # Calibrate the Microphone to Silent Levels
+        print("A moment of silence, please...")
+        with m as source:
+            r.adjust_for_ambient_noise(source)
+            print("Set minimum energy threshold to {}".format(r.energy_threshold))
+        # Create a root widget object and return as root
+        return Root()
 
-    def on_pause(self):
-        return True
 
-
-if __name__ == "__main__":
+# When Executed from the command line (not imported as module), create a new SpeechApp
+if __name__ == '__main__':
     SpeechApp().run()
