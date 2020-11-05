385f9c80252336434811e5e3c758e932bfb94f70
Haknazar117 <67150813+Haknazar117@users.noreply.github.com>
2020-11-05 18:44:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -27,17 +27,20 @@ class Recorder(BoxLayout):
     OutputFormat = autoclass('android.media.MediaRecorder$OutputFormat')
     AudioEncoder = autoclass('android.media.MediaRecorder$AudioEncoder')
     
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.mRecorder = self.MediaRecorder()
-        self.mRecorder.setAudioSource(AudioSource.MIC)
-        self.mRecorder.setOutputFormat(OutputFormat.MPEG_3)
-        self.mRecorder.setOutputFile('/sdcard/testrecorder.3gp')
-        self.mRecorder.setAudioEncoder(AudioEncoder.AMR_NB)
-        self.mRecorder.prepare()
+    def listen(self):
+        mRecorder = self.MediaRecorder()
+        mRecorder.setAudioSource(AudioSource.MIC)
+        mRecorder.setOutputFormat(OutputFormat.THREE_GP)
+        mRecorder.setOutputFile('/sdcard/testrecorder.3gp')
+        mRecorder.setAudioEncoder(AudioEncoder.AMR_NB)
+        mRecorder.prepare()
+        
+        mRecorder.start()
+        time.sleep(5)
+        mRecorder.stop()
     
     def recognize(self):
-        with sr.AudioFile("test.wav") as source:
+        with sr.AudioFile("/sdcard/testrecorder.3gp") as source:
             audio = r.listen(source)
 
         try:
